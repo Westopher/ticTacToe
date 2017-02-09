d2ebea7220132138c974748d26fd4b627a2c8663
West Kraemer <WestKraemer@gmail.com>
2017-02-09 10:52:53 -0500

    add -for in- loop to check if winning combination is present. If so, then turn activeGame to false.

--- ticTacToe/ViewController.swift
+++ ticTacToe/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var activeGame = true
+    
     // 1 is noughts, 2 is crosses
     var activePlayer = 1
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 is empty, 1 is noughts, 2 is crosses
     
+    let winningCombinations = [[0, 1, 2], [3, 4, 5,], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    
     @IBAction func buttonPressed(_ sender: AnyObject) {
         
         let activePosition = sender.tag - 1
         
-        if gameState[activePosition] == 0 {
+        if gameState[activePosition] == 0 && activeGame {
             
             gameState[activePosition] = activePlayer
         
@@ -33,6 +37,19 @@ class ViewController: UIViewController {
                 sender.setImage(UIImage(named: "cross.png"), for: [])
                 activePlayer = 1
             }
+            
+            for combination in winningCombinations {
+                
+                //If this is true, then there is a winner.
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                
+                    activeGame = false
+                    
+                    print(gameState[combination[0]])
+                
+                }
+            }
+            
         }
         
     }
